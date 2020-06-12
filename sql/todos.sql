5ff79f9f9c98d9f584e3a84b74db68eee5ca6184
Herve <dockerlite@gmail.com>
2020-06-12 08:13:48 +0200

    missing autoincrement on primary key

--- sql/todos.sql
+++ sql/todos.sql
@@ -1,12 +1,12 @@
 CREATE TABLE todos
 (
-    todo_id     INT PRIMARY KEY NOT NULL,
+    todo_id     INT PRIMARY KEY AUTOINCREMENT ,
     title       TEXT            NOT NULL,
     text        TEXT            NOT NULL,
     done        CHAR(50),
     pub_date    DATETIME           NOT NULL
 );
 
-INSERT INTO `todos` (`todo_id`, `title`, `text`, `done`, `pub_date`) VALUES
-(1, 'test', 'test', '0', '2019-11-20 10:00:00'),
-(2, 'qwerty', 'yyy', '0', '2019-11-20 12:45:45');
+INSERT INTO `todos` ( `title`, `text`, `done`, `pub_date`) VALUES
+('test', 'test', '0', '2019-11-20 10:00:00'),
+('qwerty', 'yyy', '0', '2019-11-20 12:45:45');
